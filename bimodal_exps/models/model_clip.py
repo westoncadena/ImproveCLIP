e6924664e2edf09255d95936e105b8db3f914d8a
westoncadena <106530799+westoncadena@users.noreply.github.com>
2024-11-26 12:02:36 -0600

    Update model_clip.py to include NTXent_Loss

--- bimodal_exps/models/model_clip.py
+++ bimodal_exps/models/model_clip.py
@@ -4,7 +4,7 @@ import timm
 from transformers import AutoModel, RobertaModel
 
 from models.losses import CLIP_Loss, CyCLIP_Loss, SogCLR_Loss, VICReg_Loss
-from models.losses import iSogCLR_New_v2_Loss, iSogCLR_New_v1_Loss, onlineCLR_Loss, iSogCLR_New_Loss
+from models.losses import iSogCLR_New_v2_Loss, iSogCLR_New_v1_Loss, onlineCLR_Loss, iSogCLR_New_Loss, NTXent_Loss
 
 import torch
 from torch import nn
@@ -100,6 +100,8 @@ class CLIP(nn.Module):
         elif self.ita_type == 'isogclr_new':
             self.criterion = iSogCLR_New_Loss(world_size=world_size, gamma=sogclr_gamma, rho_I=rho_I, rho_T=rho_T, tau_init=tau_init, bsz=bsz,
                                               use_temp_net=use_temp_net, feature_dim=embed_dim)
+        elif self.ita_type == 'NTXent':
+            self.criterion = NTXent_Loss(world_size=world_size, temperature=self.temp)
         else:
             raise NotImplementedError
 
